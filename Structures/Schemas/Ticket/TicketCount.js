b496846c5e8102fd98506bcbda887c55ac3526fe
Glitch (fifth-deadpan-fact) <none>
2024-04-28 04:40:37 +0000

    Import of chewyxrd/discordjs-v13-ticket-bot

--- Structures/Schemas/Ticket/TicketCount.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require("mongoose");
-const createSchema = new Schema({
-  GuildID: String,
-  TicketCount: String,
-});
-
-module.exports = model("TicketCount", createSchema, "TicketCount");
